4699a1b508340078a36a6a861f3433ca8f1112f1
AlexAlimbuyuguen <1013464@student.sae.edu.au>
2021-06-09 21:49:00 +1000

    Script Update
    - Drag and Drop script updated. Need to fix issue of dragging it in 3D.

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    private bool isDragging;
+    //private bool isDragging;
 
+    //public void OnMouseDown()
+    //{
+    //    isDragging = true;
+    //}
 
-    public void OnMouseDown()
+    //public void OnMouseUp()
+    //{
+    //    isDragging = false;
+    //}
+
+    //void Update()
+    //{
+    //    if (isDragging)
+    //    {
+    //        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+    //        transform.Translate(mousePosition);
+    //    }
+    //}
+
+    private bool dragging = false;
+    private float distance;
+    private Vector3 startDistance;
+
+    void OnMouseDown()
     {
-        isDragging = true;
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 rayPoint = ray.GetPoint(distance);
+        startDistance = transform.position - rayPoint;
     }
 
-    public void OnMouseUp()
+    void OnMouseUp()
     {
-        isDragging = false;
+        dragging = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isDragging)
+        if (dragging)
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            transform.Translate(mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint + startDistance;
         }
     }
 }
